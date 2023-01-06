ee1e09995bb98e17f82c9abe2b1fb95c57c42a12
Shivam Khandelwal <96838165+Falgunsk@users.noreply.github.com>
2023-01-06 20:37:42 +0530

    1st cal

---
+++ lnbcal.py
@@ -0,0 +1,19 @@
+a=float(input("Enter no. 1:"))
+b=float(input("Enter no. 2:"))
+c=float(input("Enter no. 3:"))
+sum=a+b+c
+sub=a-b-c
+multi=a*b*c
+div=a/b/c
+avg=(a+b+c)/3
+
+
+print('sum: ',sum)
+print('sub: ',sub)
+print('multi: ',multi)
+print('div: ',div)
+print('remainder:',a%b)
+print('avg: ',avg)
+print('percent of a: ',(a*100)/(a+b+c))
+print('percent of b: ',(b*100)/(a+b+c))
+print('percent of c: ',(c*100)/(a+b+c))
